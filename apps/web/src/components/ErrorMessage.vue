80560879c15f170f5115b13bd86e001e5b025f0a
Allan Amaral <allanmaralr@gmail.com>
2023-07-11 00:08:20 -0300

    feat: add error and loading components

---
+++ apps/web/src/components/ErrorMessage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const props = defineProps<{ error?: any }>();
+const router = useRouter();
+
+onMounted(() => {
+  console.error(props.error);
+});
+
+function handleReload() {
+  router.go(0);
+}
+</script>
+
+<template>
+  <div
+    class="mx-auto mt-60 flex w-72 flex-col items-center gap-4 rounded-xl bg-slate-800 p-4"
+  >
+    <div class="text-lg font-bold">Ops!</div>
+    <div>Algo de errado não está certo</div>
+    <button
+      class="flex h-10 items-center rounded-full bg-primary px-8"
+      @click="handleReload"
+    >
+      Atualizar página
+    </button>
+  </div>
+</template>
--- apps/web/src/layouts/PhoneLayout.vue
+++ apps/web/src/layouts/PhoneLayout.vue
@@ -5,7 +5,7 @@
     class="flex min-h-full flex-col items-center justify-center bg-slate-200"
   >
     <div
-      class="bg-background text-foreground flex h-[52rem] w-96 flex-col overflow-hidden rounded-[2.5rem] border-4 border-slate-500 shadow-2xl"
+      class="relative flex h-[52rem] w-96 flex-col overflow-hidden rounded-[2.5rem] border-4 border-slate-500 bg-background text-foreground shadow-2xl"
     >
       <slot></slot>
     </div>

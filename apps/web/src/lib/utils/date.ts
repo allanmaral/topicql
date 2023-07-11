3815ad2876a55d275a3d315cd83fe586f846ace8
Allan Amaral <allanmaralr@gmail.com>
2023-07-10 22:15:13 -0300

    fix: handle date as string from api

--- apps/web/src/lib/utils/date.ts
+++ apps/web/src/lib/utils/date.ts
@@ -1,8 +1,12 @@
-import { formatDistanceToNow as fnsFormatDistanceToNow } from 'date-fns';
+import {
+  formatDistanceToNow as fnsFormatDistanceToNow,
+  parseISO,
+} from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
-export function formatDistanceToNow(date: Date): string {
-  return fnsFormatDistanceToNow(date, {
+export function formatDistanceToNow(date: string | Date): string {
+  const dateObj = typeof date === 'string' ? parseISO(date) : date;
+  return fnsFormatDistanceToNow(dateObj, {
     addSuffix: true,
     locale: ptBR,
   });

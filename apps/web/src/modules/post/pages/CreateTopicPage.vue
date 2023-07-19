b83bfd7bd544642e86901049f8e3ee3f1d9344b1
Allan Amaral <allanmaralr@gmail.com>
2023-07-18 22:15:36 -0300

    feat: add create topic mutation

--- apps/web/src/modules/post/pages/CreateTopicPage.vue
+++ apps/web/src/modules/post/pages/CreateTopicPage.vue
@@ -1,13 +1,46 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useMutation } from '@urql/vue';
+
 import { getCurrentUser } from '@/lib/domain';
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import TopicForm from '../components/TopicForm.vue';
+import LoadingIndicator from '@/components/LoadingIndicator.vue';
+import { graphql } from '@/gql';
 
+const router = useRouter();
 const currentUser = getCurrentUser();
 
-// TODO: Create topic
-function handleSubmit(content: string) {
-  console.log('Submit form', content);
+const createTopicMutationDocument = graphql(/* GraphQL */ `
+  mutation CreateTopic($topic: TopicInput!) {
+    createTopic(topic: $topic) {
+      id
+      content
+      createdAt
+      author {
+        id
+        name
+      }
+    }
+  }
+`);
+
+const loading = ref(false);
+const { executeMutation: createTopic } = useMutation(
+  createTopicMutationDocument
+);
+
+async function handleSubmit(content: string) {
+  try {
+    loading.value = true;
+    createTopic({ topic: { content } });
+    router.push('/');
+  } catch (error) {
+    console.error('Ops...', error);
+  } finally {
+    loading.value = false;
+  }
 }
 </script>
 
@@ -16,7 +49,12 @@ function handleSubmit(content: string) {
     <template v-slot:title>Novo Tópico</template>
 
     <template #default>
-      <TopicForm :user="currentUser" @submit="handleSubmit"></TopicForm>
+      <template v-if="loading">
+        <LoadingIndicator />
+      </template>
+      <template v-else>
+        <TopicForm :user="currentUser" @submit="handleSubmit"></TopicForm>
+      </template>
     </template>
   </DefaultLayout>
 </template>

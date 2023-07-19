5fb828babd071f14c355f3c20e6c59c01e87a554
Allan Amaral <allanmaralr@gmail.com>
2023-07-18 21:30:39 -0300

    feat: change manual request to use urql

--- apps/web/src/modules/feed/pages/FeedPage.vue
+++ apps/web/src/modules/feed/pages/FeedPage.vue
@@ -1,60 +1,30 @@
 <script setup lang="ts">
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
-import { Topic } from '@/lib/domain';
-import { onMounted, ref } from 'vue';
+import { computed } from 'vue';
+import { useQuery, gql } from '@urql/vue';
 
-// TODO: Components
 import TopicList from '@/components/TopicList.vue';
 import LoadingIndicator from '@/components/LoadingIndicator.vue';
-// import ErrorMessage from '@/components/ErrorMessage.vue';
+import ErrorMessage from '@/components/ErrorMessage.vue';
 
-// TODO: Retrieve feed from server
-// const topics: Topic[] = [
-//   {
-//     id: 1,
-//     author: {
-//       id: 1,
-//       username: 'topicql',
-//       avatarUrl:
-//         'https://vectorseek.com/wp-content/uploads/2023/02/GraphQL-Logo-Vector.jpg',
-//     },
-//     content: 'Lorem ipsum',
-//     replies: [],
-//     createdAt: new Date(new Date().getTime() - 1000 * 60 * 10),
-//   },
-// ];
-
-const topics = ref<any>();
-const loading = ref(true);
-onMounted(() => {
-  fetch('http://localhost:4000/', {
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: `#graphql
-      query Feed {
-        feed {
+const { data, fetching, error } = useQuery({
+  query: gql`
+    query Feed {
+      feed {
+        id
+        content
+        createdAt
+        author {
           id
-          content
-          createdAt
-          author {
-            id
-            username
-            avatarUrl
-          }
+          username
+          avatarUrl
         }
       }
-      `,
-    }),
-    method: 'POST',
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      topics.value = json.data.feed;
-      loading.value = false;
-    });
+    }
+  `,
 });
+
+const topics = computed(() => data.value?.feed);
 </script>
 
 <template>
@@ -62,8 +32,11 @@ onMounted(() => {
     <template #title> TopicQL </template>
 
     <template #default>
-      <template v-if="loading">
+      <template v-if="fetching">
         <LoadingIndicator></LoadingIndicator>
+      </template>
+      <template v-else-if="error">
+        <ErrorMessage />
       </template>
       <template v-else-if="topics">
         <TopicList :topics="topics"></TopicList>

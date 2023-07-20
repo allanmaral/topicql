51d1903eb2df7640f5c94dfba79a04eab0ce4b74
Allan Amaral <allanmaralr@gmail.com>
2023-07-19 22:35:08 -0300

    feat: add profile feature

--- apps/web/src/modules/profile/pages/ProfilePage.vue
+++ apps/web/src/modules/profile/pages/ProfilePage.vue
@@ -1,10 +1,44 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+import { graphql } from '@/gql';
+import { useQuery } from '@urql/vue';
+
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import ProfileHeader from '../components/ProfileHeader.vue';
 import ProfileFeed from '../components/ProfileFeed.vue';
-import { Topic, User, getCurrentUser } from '@/lib/domain';
-import { useRoute } from 'vue-router';
-import { onMounted } from 'vue';
+import { getCurrentUser } from '@/lib/domain';
+import LoadingIndicator from '@/components/LoadingIndicator.vue';
+import ErrorMessage from '@/components/ErrorMessage.vue';
+
+const userProfileQueryDocument = graphql(/* GraphQL */ `
+  query Profile($userId: ID!) {
+    user(id: $userId) {
+      id
+      username
+      name
+      bio
+      avatarUrl
+      topics {
+        id
+        content
+        createdAt
+        author {
+          id
+          username
+          avatarUrl
+        }
+        replies {
+          author {
+            id
+            username
+            avatarUrl
+          }
+        }
+      }
+    }
+  }
+`);
 
 const route = useRoute();
 
@@ -12,47 +46,39 @@ function getUserId() {
   return route.params['id'] ? Number(route.params['id']) : getCurrentUser().id;
 }
 
-onMounted(() => {
-  const userId = getUserId();
-
-  console.log('Component mounted', userId);
+const { data, fetching, error } = useQuery({
+  query: userProfileQueryDocument,
+  variables: {
+    userId: <string>getUserId(),
+  },
 });
 
-// TODO: Load profile
-const profile: { user: User; topics: Topic[] } = {
-  user: {
-    id: 1,
-    username: 'topicql',
-    name: 'TopicQL',
-    bio: 'A GraphQL study',
-    avatarUrl:
-      'https://vectorseek.com/wp-content/uploads/2023/02/GraphQL-Logo-Vector.jpg',
-  },
-  topics: [
-    {
-      id: 1,
-      author: {
-        id: 1,
-        username: 'topicql',
-        avatarUrl:
-          'https://vectorseek.com/wp-content/uploads/2023/02/GraphQL-Logo-Vector.jpg',
-      },
-      content: 'Lorem ipsum',
-      replies: [],
-      createdAt: new Date(new Date().getTime() - 1000 * 60 * 10),
-    },
-  ],
-};
+const profile = computed(() =>
+  data.value?.user
+    ? {
+        user: data.value.user,
+        topics: data.value.user.topics,
+      }
+    : undefined
+);
 </script>
 
 <template>
   <DefaultLayout>
     <template #title>
-      <ProfileHeader :user="profile.user"></ProfileHeader>
+      <ProfileHeader v-if="profile" :user="profile.user"></ProfileHeader>
     </template>
 
     <template #default>
-      <ProfileFeed :topics="profile.topics"></ProfileFeed>
+      <template v-if="fetching">
+        <LoadingIndicator />
+      </template>
+      <template v-else-if="error">
+        <ErrorMessage />
+      </template>
+      <template v-else-if="profile">
+        <ProfileFeed :topics="profile.topics"></ProfileFeed>
+      </template>
     </template>
   </DefaultLayout>
 </template>

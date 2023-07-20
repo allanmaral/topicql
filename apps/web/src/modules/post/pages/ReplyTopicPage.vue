f632d280c49e959fc2b185ec6b6e1e23b04d3f4f
Allan Amaral <allanmaralr@gmail.com>
2023-07-19 22:46:02 -0300

    feat: add reply feature

--- apps/web/src/modules/post/pages/ReplyTopicPage.vue
+++ apps/web/src/modules/post/pages/ReplyTopicPage.vue
@@ -1,13 +1,47 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useMutation } from '@urql/vue';
+import { useRoute, useRouter } from 'vue-router';
+
+import { graphql } from '@/gql';
 import { getCurrentUser } from '@/lib/domain';
 import DetailLayout from '@/layouts/DetailLayout.vue';
 import TopicForm from '../components/TopicForm.vue';
+import LoadingIndicator from '@/components/LoadingIndicator.vue';
+
+const replyTopicMutationDocument = graphql(/* GraphQL */ `
+  mutation ReplyTopic($topic: TopicInput!) {
+    createTopic(topic: $topic) {
+      id
+      content
+      createdAt
+      author {
+        id
+        name
+      }
+    }
+  }
+`);
 
 const currentUser = getCurrentUser();
+const route = useRoute();
+const router = useRouter();
+const { executeMutation: replyTopic } = useMutation(replyTopicMutationDocument);
 
-// TODO: Reply a topic
-function handleSubmit(content: string) {
-  console.log('Submit form', content);
+const loading = ref(false);
+async function handleSubmit(content: string) {
+  loading.value = true;
+  try {
+    const topicId = <string>route.params['id'];
+    await replyTopic({
+      topic: { content, replyTo: topicId },
+    });
+    router.push('/');
+  } catch (error) {
+    console.error('Ops...', error);
+  } finally {
+    loading.value = false;
+  }
 }
 </script>
 
@@ -16,11 +50,16 @@ function handleSubmit(content: string) {
     <template #title>Responder</template>
 
     <template #default>
-      <TopicForm
-        :user="currentUser"
-        :reply="true"
-        @submit="handleSubmit"
-      ></TopicForm>
+      <template v-if="loading">
+        <LoadingIndicator />
+      </template>
+      <template v-else>
+        <TopicForm
+          :user="currentUser"
+          :reply="true"
+          @submit="handleSubmit"
+        ></TopicForm>
+      </template>
     </template>
   </DetailLayout>
 </template>
